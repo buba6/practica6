89382d232c89d1291e313e25ebdca61be26d024c
chuyano <spawner.astral@hotmail.com>
2015-10-25 14:38:50 -0600

    1

---
+++ newfile.php
@@ -0,0 +1,22 @@
+
+
+<html>
+<body>
+<form action="sesion.php" method="post">
+
+<p align="center"><label>usuario<label/>
+<br/>
+<br/>
+<input type="text" name="name">
+<br/>
+<br/>
+<label>password<label/>
+<br/>
+<br/>
+<input type="password" name="pass">
+<br/>
+<br/>
+<input type="submit" value="Entrar">
+</form>
+<body/>
+<html>
